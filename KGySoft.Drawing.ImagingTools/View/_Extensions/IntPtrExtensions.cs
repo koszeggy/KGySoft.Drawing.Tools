728302fd73321d9d2daf3a8745e79ef438517d53
György Kőszeg <koszeggy@yahoo.com>
2020-05-03 12:07:35 +0200

    Fixing possible overflow exception

--- KGySoft.Drawing.ImagingTools/View/_Extensions/IntPtrExtensions.cs
+++ KGySoft.Drawing.ImagingTools/View/_Extensions/IntPtrExtensions.cs
@@ -4,7 +4,7 @@ namespace KGySoft.Drawing.ImagingTools.View
 {
     internal static class IntPtrExtensions
     {
-        internal static int GetSignedLoWord(this IntPtr p) => (short)((int)p & 0xFFFF);
-        internal static int GetSignedHiWord(this IntPtr p) => (short)(((int)p >> 16) & 0xFFFF);
+        internal static int GetSignedLoWord(this IntPtr p) => (short)(p.ToInt64() & 0xFFFF);
+        internal static int GetSignedHiWord(this IntPtr p) => (short)((p.ToInt64() >> 16) & 0xFFFF);
     }
 }
