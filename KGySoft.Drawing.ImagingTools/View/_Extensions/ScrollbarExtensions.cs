e7df68952e92efeda763a9e22289741b8903c3b3
György Kőszeg <koszeggy@gmail.com>
2021-05-13 18:34:46 +0200

    Formatting

--- KGySoft.Drawing.ImagingTools/View/_Extensions/ScrollbarExtensions.cs
+++ KGySoft.Drawing.ImagingTools/View/_Extensions/ScrollbarExtensions.cs
@@ -1,9 +1,31 @@
-﻿using System.Windows.Forms;
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: ScrollbarExtensions.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2021 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution. If not, then this file is considered as
+//  an illegal copy.
+//
+//  Unauthorized copying of this file, via any medium is strictly prohibited.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+using System.Windows.Forms;
+
+#endregion
 
 namespace KGySoft.Drawing.ImagingTools.View
 {
     internal static class ScrollbarExtensions
     {
+        #region Methods
+
         internal static void SetValueSafe(this ScrollBar scrollBar, int value)
         {
             if (value < scrollBar.Minimum)
@@ -13,5 +35,7 @@ namespace KGySoft.Drawing.ImagingTools.View
 
             scrollBar.Value = value;
         }
+
+        #endregion
     }
 }
