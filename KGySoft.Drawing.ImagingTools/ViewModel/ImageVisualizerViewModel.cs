8d64fdb3263e9f1d643d3cc2bcc56677d0d2dcbe
György Kőszeg <koszeggy@gmail.com>
2021-09-18 11:22:16 +0200

    Fixing opening URLs in .NET 5

--- KGySoft.Drawing.ImagingTools/ViewModel/ImageVisualizerViewModel.cs
+++ KGySoft.Drawing.ImagingTools/ViewModel/ImageVisualizerViewModel.cs
@@ -155,10 +155,10 @@ namespace KGySoft.Drawing.ImagingTools.ViewModel
         internal ICommand AdjustContrastCommand => Get(() => new SimpleCommand(OnAdjustContrastCommand));
         internal ICommand AdjustGammaCommand => Get(() => new SimpleCommand(OnAdjustGammaCommand));
         internal ICommand ShowAboutCommand => Get(() => new SimpleCommand(OnShowAboutCommand));
-        internal ICommand VisitWebSiteCommand => Get(() => new SimpleCommand(() => Process.Start("https://kgysoft.net")));
-        internal ICommand VisitGitHubCommand => Get(() => new SimpleCommand(() => Process.Start("https://github.com/koszeggy/KGySoft.Drawing.Tools")));
-        internal ICommand VisitMarketplaceCommand => Get(() => new SimpleCommand(() => Process.Start("https://marketplace.visualstudio.com/items?itemName=KGySoft.drawing-debugger-visualizers")));
-        internal ICommand SubmitResourcesCommand => Get(() => new SimpleCommand(() => Process.Start("https://github.com/koszeggy/KGySoft.Drawing.Tools/issues/new?assignees=&labels=&template=submit-resources.md&title=%5BRes%5D")));
+        internal ICommand VisitWebSiteCommand => Get(() => new SimpleCommand(() => OpenUrl("https://kgysoft.net")));
+        internal ICommand VisitGitHubCommand => Get(() => new SimpleCommand(() => OpenUrl("https://github.com/koszeggy/KGySoft.Drawing.Tools")));
+        internal ICommand VisitMarketplaceCommand => Get(() => new SimpleCommand(() => OpenUrl("https://marketplace.visualstudio.com/items?itemName=KGySoft.drawing-debugger-visualizers")));
+        internal ICommand SubmitResourcesCommand => Get(() => new SimpleCommand(() => OpenUrl("https://github.com/koszeggy/KGySoft.Drawing.Tools/issues/new?assignees=&labels=&template=submit-resources.md&title=%5BRes%5D")));
         internal ICommand ShowEasterEggCommand => Get(() => new SimpleCommand(() => ShowInfo(Res.InfoMessageEasterEgg)));
 
         #endregion
@@ -233,6 +233,8 @@ namespace KGySoft.Drawing.ImagingTools.ViewModel
             image = bitmapData.ToBitmap();
             return true;
         }
+
+        private static void OpenUrl(string url) => Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
 
         #endregion
 
