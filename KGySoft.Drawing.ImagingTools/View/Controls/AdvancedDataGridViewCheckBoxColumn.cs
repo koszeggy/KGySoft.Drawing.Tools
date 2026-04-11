be9982ad794b2598df18d2a5eeb01de9f1276bde
György Kőszeg <koszeggy@gmail.com>
2026-04-11 20:53:38 +0200

    AdvancedCheckBoxColumn: Customized tool tip

--- KGySoft.Drawing.ImagingTools/View/Controls/AdvancedDataGridViewCheckBoxColumn.cs
+++ KGySoft.Drawing.ImagingTools/View/Controls/AdvancedDataGridViewCheckBoxColumn.cs
@@ -15,6 +15,8 @@
 
 #region Usings
 
+using System;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 using KGySoft.WinForms;
@@ -24,10 +26,35 @@ using KGySoft.WinForms;
 namespace KGySoft.Drawing.ImagingTools.View.Controls
 {
     /// <summary>
-    /// A CheckBox column with per-monitor DPI and dark mode support. Standard (= System) style only.
+    /// A CheckBox column that
+    /// - Supports per-monitor DPI
+    /// - Supports dark mode (Standard/System) style only.
+    /// - Takes the tool tip from resources instead of constant True/False
     /// </summary>
     internal class AdvancedDataGridViewCheckBoxColumn : DataGridViewCheckBoxColumn
     {
+        #region Fields
+
+        private DataGridView? grid;
+
+        #endregion
+
+        #region Properties
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public new FlatStyle FlatStyle
+        {
+            get => FlatStyle.Standard;
+            set
+            {
+                if (value is not (FlatStyle.Standard or FlatStyle.System))
+                    throw new ArgumentOutOfRangeException(nameof(value), PublicResources.ArgumentOutOfRange);
+            }
+        }
+
+        #endregion
+
         #region Constructors
 
         public AdvancedDataGridViewCheckBoxColumn()
@@ -51,6 +78,54 @@ namespace KGySoft.Drawing.ImagingTools.View.Controls
                 };
             }
         }
+
+        #endregion
+
+        #region Methods
+
+        #region Protected Methods
+        
+        protected override void OnDataGridViewChanged()
+        {
+            base.OnDataGridViewChanged();
+            if (DataGridView?.IsDisposed != false)
+                return;
+
+            grid?.CellToolTipTextNeeded -= DataGridView_CellToolTipTextNeeded;
+            DataGridView?.CellToolTipTextNeeded += DataGridView_CellToolTipTextNeeded;
+            grid = DataGridView;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            grid?.CellToolTipTextNeeded -= DataGridView_CellToolTipTextNeeded;
+            grid = null;
+            base.Dispose(disposing);
+        }
+
+        #endregion
+
+        #region Event Handlers
+        
+        private void DataGridView_CellToolTipTextNeeded(object? sender, DataGridViewCellToolTipTextNeededEventArgs e)
+        {
+            Debug.Assert(sender == DataGridView);
+            if (DataGridView is null || e.ColumnIndex < 0 || e.RowIndex < 0 || e.ColumnIndex >= DataGridView.ColumnCount || DataGridView.Columns[e.ColumnIndex] != this)
+                return;
+
+            // already set
+            if (!String.IsNullOrEmpty(e.ToolTipText))
+                return;
+
+            e.ToolTipText = DataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value switch
+            {
+                true or CheckState.Checked => Res.Get(CheckState.Checked),
+                false or CheckState.Unchecked => Res.Get(CheckState.Unchecked),
+                _ => Res.Get(CheckState.Indeterminate)
+            };
+        }
+
+        #endregion
 
         #endregion
     }
