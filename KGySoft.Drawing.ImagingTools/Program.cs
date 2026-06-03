dacb341620330878c7e147e83a940beafe8c0423
György Kőszeg <koszeggy@gmail.com>
2026-06-03 21:05:51 +0200

    ImagingTools: Displaying environment info in the Console in Debug mode

--- KGySoft.Drawing.ImagingTools/Program.cs
+++ KGySoft.Drawing.ImagingTools/Program.cs
@@ -16,10 +16,17 @@
 #region Usings
 
 using System;
+using System.Diagnostics;
+#if NET471_OR_GREATER || NETCOREAPP
+using System.Runtime.InteropServices;
+#endif
+using System.Runtime.Versioning;
 using System.Windows.Forms;
 
 using KGySoft.Drawing.ImagingTools.View;
 using KGySoft.Drawing.ImagingTools.ViewModel;
+using KGySoft.Reflection;
+using KGySoft.WinForms;
 
 #endregion
 
@@ -33,15 +40,35 @@ namespace KGySoft.Drawing.ImagingTools
         /// When executed as a standalone application, this is the entry point.
         /// </summary>
         [STAThread]
-        internal static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             ThemeColors.SetBaseTheme(DefaultTheme.System);
+            DumpDebugInfo();
 
             using IViewModel viewModel = ViewModelFactory.FromCommandLineArguments(args);
             using IView view = ViewFactory.CreateView(viewModel);
             Application.Run(ViewFactory.TryGetForm(view)!);
+        }
+
+        [Conditional("DEBUG")]
+        private static void DumpDebugInfo()
+        {
+#if NET35
+            const string frameworkName = ".NET Framework 3.5";
+#else
+            TargetFrameworkAttribute attr = (TargetFrameworkAttribute)Attribute.GetCustomAttribute(typeof(Program).Assembly, typeof(TargetFrameworkAttribute))!;
+            string frameworkName = attr.FrameworkDisplayName is { Length: > 0 } name ? name : attr.FrameworkName;
+#endif
+            Console.WriteLine(frameworkName);
+            Console.WriteLine($"IsWindows: {OSHelper.IsWindows} {OSHelper.GetWindowsVersion()}");
+            Console.WriteLine($"IsMono: {OSHelper.IsMono} {(OSHelper.IsMono ? Reflector.InvokeMethod(Type.GetType("Mono.Runtime")!, "GetDisplayName") : null)}");
+            Console.WriteLine($"IsWine: {OSHelper.IsWine} {Environment.GetEnvironmentVariable("WINEPREFIX")}");
+            Console.WriteLine($"Environment.OSVersion.Platform: {Environment.OSVersion.Platform}");
+#if NET471_OR_GREATER || NETCOREAPP
+            Console.WriteLine($"RuntimeInformation.FrameworkDescription: {RuntimeInformation.FrameworkDescription}");
+#endif  
         }
 
         #endregion
