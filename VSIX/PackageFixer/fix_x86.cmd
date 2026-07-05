2b8ec878c64310c4c66909fdd28de21931faf7c2
György Kőszeg <koszeggy@gmail.com>
2026-07-05 18:02:48 +0200

    Releasing version v6.0.0

--- VSIX/PackageFixer/fix_x86.cmd
+++ VSIX/PackageFixer/fix_x86.cmd
@@ -1,1 +1,1 @@
-bin\Release\net9.0\PackageFixer ..\KGySoft.Drawing.DebuggerVisualizers.Package.x86\bin\Release\KGySoft.Drawing.DebuggerVisualizers.Package.vsix D:\Temp\vsix\ref.x86.vsix
+bin\Release\net10.0\PackageFixer ..\KGySoft.Drawing.DebuggerVisualizers.Package.x86\bin\Release\KGySoft.Drawing.DebuggerVisualizers.Package.vsix D:\Temp\vsix\ref.x86.vsix
